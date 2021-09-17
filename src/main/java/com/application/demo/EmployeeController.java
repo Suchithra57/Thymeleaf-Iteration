1216dbe4619d969df9f93d85317c7cc4ca6bcf2e
Suchithra57 <suchithrakarlapudi@gmail.com>
2021-09-17 10:44:01 +0530

    Thymeleaf Iteration

---
+++ src/main/java/com/application/demo/EmployeeController.java
@@ -0,0 +1,65 @@
+package com.application.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+	@GetMapping("/EmployeeData")
+    public String iteration(Model model) {
+        List< Employee > employees = new ArrayList< > ();
+        employees.add(new Employee("Ramesh", "Fadatare", "ramesh@gmail.com"));
+        employees.add(new Employee("John", "Cena", "john@gmail.com"));
+        employees.add(new Employee("Tom", "Cruise", "tom@gmail.com"));
+        employees.add(new Employee("Tony", "Stark", "tony@gmail.com"));
+        
+        model.addAttribute("employees", employees);
+        return "iteration";
+    }
+	
+	
+	@GetMapping("/arraysDemo")
+    public String arraysDemo(Model model) {
+        String[] planets = {
+            "MERCURY",
+            "VENUS",
+            "EARTH",
+            "MARS",
+            "JUPITER",
+            "SATURN",
+            "URANUS",
+            "NEPTUNE"
+        };
+        
+		/*
+		 * Arrays.sort(planets); for (String planet : planets) {
+		 * System.out.println(planet); }
+		 */
+        
+        model.addAttribute("planets", planets);
+        return "array-demo";
+	}
+	
+	@GetMapping("/print")
+	public void print()
+	{
+		 List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+	        Iterator<Integer> iterator = list.iterator();
+
+	        while (iterator.hasNext()) {
+
+	            Integer result = iterator.next();
+	            System.out.println(result);
+
+	        }
+	        
+	}
+	
+}
